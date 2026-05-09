3fcf4bea1b5670ba55bba33fc3932b58e3fdd5ae
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-05-08 23:05:21 -0400

    header for Configuration.h created

---
+++ src/main/include/Configuration.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+namespace Configuration {
+    constexpr int teamNumber = 1369;
+    constexpr int networkTablePollHz = 20;
+
+    namespace NT {
+        constexpr const char* fmsTable = "FMSInfo";
+        constexpr const char* isRedAlliance = "IsRedAlliance";
+    }
+
+    constexpr const char* serialPort = "";
+    constexpr int baudRate = 115200;
+
+    constexpr int ledCount = 110;
+}
