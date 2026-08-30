3d1993a529c98d14b16ffd4d6f6d8dc5f28ee8c2
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-30 00:40:13 -0400

    properly import files now!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod color;
 mod animation {
-    mod solid_animation;
-    mod strobe_animation;
-    mod larson_animation;
-    mod rainbow_animation;
+    pub mod solid_animation;
+    pub mod strobe_animation;
+    pub mod larson_animation;
+    pub mod rainbow_animation;
 }
 
 use crate::color::RGBWColor;
-// use crate::animation::SolidAnimation
+use crate::animation::solid_animation::SolidAnimation;
+use crate::animation::strobe_animation::StrobeAnimation;
+use crate::animation::larson_animation::LarsonAnimation;
+use crate::animation::rainbow_animation::RainbowAnimation;
 
 fn main() {
     
